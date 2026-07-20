34be62c6051557bf441a7033e6a0d1e424e84a7e
sanya pal <141217594+SANYAPAL26@users.noreply.github.com>
2026-07-20 17:55:14 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = [-1]
+        max_length = 0
+
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+
+                if not stack:
+                    stack.append(i)
+                else:
+                    max_length = max(max_length, i - stack[-1])
+
+        return max_length
+
+
+# Example
+if __name__ == "__main__":
+    sol = Solution()
+
+    print(sol.longestValidParentheses("(()"))      # 2
+    print(sol.longestValidParentheses(")()())"))   # 4
+    print(sol.longestValidParentheses(""))         # 0
